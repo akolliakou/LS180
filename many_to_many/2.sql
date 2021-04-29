d5a1edf39c8e28768efcdd0ba74ffc931c2d286b
Anna Kolliakou <akolliakou@gmail.com>
2021-04-29 16:57:40 +0100

    More M:M exercises

---
+++ many_to_many/2.sql
@@ -0,0 +1,6 @@
+-- Write a query to retrieve the customer data for every customer 
+-- who currently subscribes to at least one service.
+
+SELECT DISTINCT customers.* FROM customers
+JOIN customers_services 
+ON customers_services.customer_id = customers.id;
