67beab453c64f727fd6a1ff69d72e96d82a1d515
Anna Kolliakou <akolliakou@gmail.com>
2021-05-04 18:38:38 +0100

    More M:M exercises

---
+++ many_to_many/10.sql
@@ -0,0 +1,13 @@
+-- Write the necessary SQL statements to delete the "Bulk Email" 
+-- service and customer "Chen Ke-Hua" from the database.
+
+DELETE FROM customers 
+WHERE name = 'Chen Ke-Hua';
+--
+DELETE FROM customers_services
+WHERE service_id IN
+(SELECT id FROM services 
+WHERE description = 'Bulk Email');
+--
+DELETE FROM services
+WHERE description = 'Bulk Email';
